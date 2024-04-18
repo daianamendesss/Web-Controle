ca097739f312ce775c1733e805fead9c60633967
Daiana Mendes <103112318+daianamendesss@users.noreply.github.com>
2024-04-18 09:54:58 -0300

    Add files via upload

---
+++ Trabalho Emerson/web-controle-2024/controle.js
@@ -0,0 +1,73 @@
+const form = document.getElementById("form");
+const descInput = document.getElementById("descricao");
+const valorInput = document.querySelector("#montante");
+const balancoH1 = document.getElementById("balanco");
+const receitaP = document.querySelector("#din-positivo");
+const despesaP = document.querySelector("#din-negativo");
+const transacoesUL = document.getElementById("transacoes");
+
+// Terá outras coisas
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const descTransacao = descInput.value.trim();
+  const valorTransacao = valorInput.value.trim();
+
+  if (descTransacao == "") {
+    alert("Informe a descrião da transação!");
+    descInput.focus();
+    return;
+  }
+  if (valorTransacao == "") {
+    alert("Informe o valor da transação!");
+    valorInput.focus();
+    return;
+  }
+
+  const transacao = {
+    id: parseInt(Math.random() * 10000),
+    desc: descTransacao,
+    valor: parseFloat(valorTransacao),
+  };
+
+  somaAoSaldo(transacao);
+  somaRecitaDespesa(transacao);
+  addTransacaoAoDOM(transacao);
+
+  descInput.value = "";
+  valorInput.value = "";
+});
+
+function somaAoSaldo(transacao) {
+  let valorBalanco = balancoH1.innerHTML.trim();
+  valorBalanco = valorBalanco.replace("R$", "");
+
+  valorBalanco = parseFloat(valorBalanco);
+  valorBalanco += transacao.valor;
+
+  balancoH1.innerHTML = `R$${valorBalanco.toFixed(2)}`;
+}
+
+function somaRecitaDespesa(transacao) {
+  const elemento = transacao.valor > 0 ? receitaP : despesaP;
+  const substituir = transacao.valor > 0 ? "+ R$" : "- R$";
+  let valor = elemento.innerHTML.replace(substituir, "");
+  valor = parseFloat(valor);
+  valor += Math.abs(transacao.valor);
+
+  elemento.innerHTML = `${substituir}${valor.toFixed(2)}`;
+}
+
+function addTransacaoAoDOM(transacao) {
+  const cssClass = transacao.valor > 0 ? "positivo" : "negativo";
+
+  const currency = transacao.valor > 0 ? "R$" : "-R$";
+
+  const liElementStr = `${transacao.desc} <span>${currency}${Math.abs(transacao.valor)}</span><button class="delete-btn">X</button>`;
+
+  const liElement = document.createElement("li");
+  liElement.classList.add(cssClass);
+  liElement.innerHTML = liElementStr;
+  transacoesUL.append(liElement);
+}
